dbac54d74e40d225aaac3e15dc7f942999add935
Ankur godara <90837436+kiraxGODARA@users.noreply.github.com>
2022-01-09 03:39:14 +0530

    Update a.traversal.cpp

--- Binary Trees/a.traversal.cpp
+++ Binary Trees/a.traversal.cpp
@@ -89,3 +89,59 @@ void preorder(node* root){
  
     }
 }
+
+
+2>  POSTORDER
+
+i)recursive
+
+void postorder(node* root){
+  if(!root)
+    return;
+  inorder(root->left); 
+  inorder(root->right);
+  cout << root->data;
+}
+
+ii)iterative
+
+void postorder(Node* root)
+{
+    // return if the tree is empty
+    if (root == nullptr) {
+        return;
+    }
+ 
+    // create an empty stack and push the root node
+    stack<Node*> s;
+    s.push(root);
+ 
+    // create another stack to store postorder traversal
+    stack<int> out;
+ 
+    // loop till stack is empty
+    while (!s.empty())
+    {
+        // pop a node from the stack and push the data into the output stack
+        Node* front = s.top();
+        s.pop();
+ 
+        out.push(front->data);
+ 
+        // push the left and right child of the popped node into the stack
+        if (front->left) {
+            s.push(front->left);
+        }
+ 
+        if (front->right) {
+            s.push(front->right);
+        }
+    }
+    // print postorder traversal
+    while (!out.empty())
+    {
+        cout << out.top() << " ";
+        out.pop();
+    }
+}
+
