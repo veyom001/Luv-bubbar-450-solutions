27c4bf811eba6432de33d376d9772c3e666886ba
Ankur godara <90837436+kiraxGODARA@users.noreply.github.com>
2022-01-14 18:32:47 +0530

    Update a.stack_implement.cpp

--- Stack/a.stack_implement.cpp
+++ Stack/a.stack_implement.cpp
@@ -1,19 +1,25 @@
+// we define maxsize that the stack may take. also an array which store stack's data and a top that keeps the pointer to the top element of the stock.
+
 public class MyStack {
    private int maxSize;
    private long[] stackArray;
    private int top;
    
+  // making mystack custom constructor
    public MyStack(int s) {
       maxSize = s;
       stackArray = new long[maxSize];
       top = -1;
    }
+  // note: we used ++top so that first we increment top from -1 to 0 and push our element and keeps the top to 1 now.
    public void push(long j) {
       stackArray[++top] = j;
    }
    public long pop() {
       return stackArray[top--];
    }
+  // peek returns the top of our stack
+  
    public long peek() {
       return stackArray[top];
    }
