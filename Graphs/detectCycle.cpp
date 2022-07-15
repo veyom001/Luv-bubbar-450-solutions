eac5f8bae45bb952d49cca51903ef588f766969b
veyom001 <64396955+veyom001@users.noreply.github.com>
2022-07-15 15:51:15 +0530

    Update detectCycle.cpp

--- Graphs/detectCycle.cpp
+++ Graphs/detectCycle.cpp
@@ -1,4 +1,6 @@
-   bool ContainCycle(int current,vector<bool>&visited,vector<bool>&recStack,vector<int>adj[])
+  
+//DIRECTED GRAPH
+bool ContainCycle(int current,vector<bool>&visited,vector<bool>&recStack,vector<int>adj[])
     {
         if(visited[current]==false)
         {
@@ -36,6 +38,49 @@
         }
     }
         
+        return false;
+        
+    }
+
+
+//UNDIRECTED GRAPH
+
+bool findCycleUndirected(int current,int parent,vector<int>adj[],vector<bool>&visited)
+  {
+      
+      visited[current]=true;
+      
+      for(auto it:adj[current])
+      {
+          if(visited[it]==false)
+          {
+              if(findCycleUndirected(it,current,adj,visited))
+           return true;   
+          }
+          else if(it!=parent)
+          {
+              return true;
+          }
+      }
+      return false;
+      
+      
+      
+      
+  }
+    // Function to detect cycle in an undirected graph.
+    bool isCycle(int V, vector<int> adj[]) {
+        // Code here
+        
+        vector<bool>visited(V+1,false);
+        
+        for(int i=0;i<V;i++)
+        {
+            if(!visited[i]&&findCycleUndirected(i,-1,adj,visited))
+            {
+                return true;
+            }
+        }
         return false;
         
     }
